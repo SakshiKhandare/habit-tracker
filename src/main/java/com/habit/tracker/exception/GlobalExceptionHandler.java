2915b1212354a6d7acc1feba855eb1713ef0b7c7
SakshiKhandare <khandaresakshi27@gmail.com>
2025-11-20 23:46:25 +0530

    fix issue in GlobalExceptionHandler

--- src/main/java/com/habit/tracker/exception/GlobalExceptionHandler.java
+++ src/main/java/com/habit/tracker/exception/GlobalExceptionHandler.java
@@ -3,12 +3,22 @@ package com.habit.tracker.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+  GlobalExceptionHandler
+  - Catches validation errors thrown by @Valid.
+  - Returns a simple JSON map: { "fieldName": "error message" }.
+*/
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>> handleValidationErrors(MethodArgumentNotValidException ex){
 
         Map<String, String> errors = new HashMap<>();
